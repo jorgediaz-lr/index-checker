0932d80f30ceea12a969ee32e7194864ddea32c3
Jorge Díaz <jorge.diaz@liferay.com>
2015-12-14 16:26:47 +0100

    Add copyright information

--- src/com/jorgediaz/util/model/DefaultModel.java
+++ src/com/jorgediaz/util/model/DefaultModel.java
@@ -1,9 +1,22 @@
 /**
- * Space for Copyright
+ * Copyright (c) 2015-present Jorge Díaz All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  */
 
 package com.jorgediaz.util.model;
 
+/**
+ * @author Jorge Díaz
+ */
 public class DefaultModel extends ModelImpl {
 
 }
