bea7f2103621a99dc5205f6493f9900d894e5a0b
Jorge Díaz <jorge.diaz@liferay.com>
2016-07-03 22:23:15 +0200

    Add missing import

--- src/jorgediazest/util/output/OutputUtils.java
+++ src/jorgediazest/util/output/OutputUtils.java
@@ -48,6 +48,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.Set;
 
 import javax.portlet.PortletConfig;
