f531751e13589df8e9e1fc742457eece86184a55
Jorge Díaz <jorge.diaz@liferay.com>
2016-12-20 02:06:22 +0100

    Add new addPortletFileEntry, cleanupPortletFileEntries, getPortletFileEntry and servePortletFileEntry methods

--- src/jorgediazest/util/output/OutputUtils.java
+++ src/jorgediazest/util/output/OutputUtils.java
@@ -17,17 +17,75 @@ package jorgediazest.util.output;
 /**
  * @author Jorge Díaz
  */
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.servlet.HttpHeaders;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.Company;
+import com.liferay.portal.model.Repository;
+import com.liferay.portal.portletfilerepository.PortletFileRepositoryUtil;
+import com.liferay.portal.service.CompanyLocalServiceUtil;
+import com.liferay.portal.service.GroupServiceUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portlet.documentlibrary.NoSuchFileEntryException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 import javax.portlet.PortletConfig;
+import javax.portlet.ResourceResponse;
+
+import javax.servlet.http.HttpServletResponse;
 public class OutputUtils {
+
+	public static FileEntry addPortletFileEntry(
+			Repository repository, InputStream inputStream, long userId,
+			String title, String mimeType)
+		throws PortalException, SystemException {
+
+		if (Validator.isNull(inputStream)) {
+			return null;
+		}
+
+		return PortletFileRepositoryUtil.addPortletFileEntry(
+			repository.getGroupId(), userId, StringPool.BLANK, 0,
+			repository.getPortletId(), repository.getDlFolderId(), inputStream,
+			title, mimeType, true);
+	}
+
+	public static void cleanupPortletFileEntries(
+			Repository repository, long minutes)
+		throws PortalException, SystemException {
+
+		if (repository == null) {
+			return;
+		}
+
+		List<FileEntry> fileEntries =
+			PortletFileRepositoryUtil.getPortletFileEntries(
+				repository.getGroupId(), repository.getDlFolderId());
+
+		for (FileEntry fileEntry : fileEntries) {
+			long fileEntryDate = fileEntry.getCreateDate().getTime();
+			long delta = minutes * 60 *1000;
+
+			if ((fileEntryDate + delta) < System.currentTimeMillis()) {
+				PortletFileRepositoryUtil.deletePortletFileEntry(
+					fileEntry.getFileEntryId());
+			}
+		}
+	}
 
 	public static String getCSVRow(List<String> rowData) {
 		return getCSVRow(rowData, StringPool.COMMA);
@@ -56,6 +114,34 @@ public class OutputUtils {
 		return headers;
 	}
 
+	public static FileEntry getPortletFileEntry(
+		Repository repository, String title)
+			throws PortalException, SystemException {
+
+		return PortletFileRepositoryUtil.getPortletFileEntry(
+			repository.getGroupId(), repository.getDlFolderId(), title);
+	}
+
+	public static Repository getPortletRepository(String portletId)
+		throws PortalException, SystemException {
+
+		List<Company> companies = CompanyLocalServiceUtil.getCompanies(false);
+
+		long companyId = companies.get(0).getCompanyId();
+		long groupId = GroupServiceUtil.getCompanyGroup(companyId).getGroupId();
+
+		Repository repository =
+			PortletFileRepositoryUtil.fetchPortletRepository(
+				groupId, portletId);
+
+		if (repository == null) {
+			repository = PortletFileRepositoryUtil.addPortletRepository(
+				groupId, portletId, new ServiceContext());
+		}
+
+		return repository;
+	}
+
 	public static String listStringToString(List<String> out) {
 
 		if (Validator.isNull(out)) {
@@ -72,6 +158,48 @@ public class OutputUtils {
 		return stringBundler.toString();
 	}
 
+	public static void servePortletFileEntry(
+			String portletId, String title, ResourceResponse response)
+		throws IOException {
+
+		try {
+			Repository repository = OutputUtils.getPortletRepository(portletId);
+
+			FileEntry fileEntry = OutputUtils.getPortletFileEntry(
+				repository, title);
+
+			InputStream inputStream = fileEntry.getContentStream();
+			OutputStream outputStream = response.getPortletOutputStream();
+
+			byte[] buffer = new byte[10024];
+			int bytesRead = 0;
+			while ((bytesRead = inputStream.read(buffer)) != -1) {
+				outputStream.write(buffer, 0, bytesRead);
+			}
+
+			response.setContentType(fileEntry.getMimeType());
+
+			response.addProperty(
+				HttpHeaders.CONTENT_DISPOSITION, "attachment; filename="+title);
+		}
+		catch (NoSuchFileEntryException nsfe) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(nsfe.getMessage());
+			}
+
+			response.setProperty(
+				ResourceResponse.HTTP_STATUS_CODE,
+				Integer.toString(HttpServletResponse.SC_NOT_FOUND));
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			response.setProperty(
+				ResourceResponse.HTTP_STATUS_CODE,
+				Integer.toString(HttpServletResponse.SC_INTERNAL_SERVER_ERROR));
+		}
+	}
+
 	protected static String addCell(String line, String cell, String sep) {
 		if (cell.contains(StringPool.SPACE) || cell.contains(sep)) {
 			cell = StringPool.QUOTE + cell + StringPool.QUOTE;
@@ -86,5 +214,7 @@ public class OutputUtils {
 
 		return line;
 	}
+
+	private static Log _log = LogFactoryUtil.getLog(OutputUtils.class);
 
 }
