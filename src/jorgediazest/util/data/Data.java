26d35ca0cd2ac9465a382c68748924e4daeb5d48
Jorge Díaz <jorge.diaz@liferay.com>
2017-07-30 18:59:17 +0200

    Add missing import

--- src/jorgediazest/util/data/Data.java
+++ src/jorgediazest/util/data/Data.java
@@ -26,6 +26,7 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
 import jorgediazest.util.comparator.DataComparator;
 import jorgediazest.util.model.Model;
