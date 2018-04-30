7ab8ba82c4882d2e2f808318d9201bafb37a4ebb
Jorge Díaz <jorge.diaz@liferay.com>
2018-04-30 21:44:05 +0200

    Fix status filter, in order to avoid problems with new modified date filter

--- docroot/WEB-INF/src/jorgediazest/indexchecker/model/JournalArticleQueryHelper.java
+++ docroot/WEB-INF/src/jorgediazest/indexchecker/model/JournalArticleQueryHelper.java
@@ -121,16 +121,16 @@ public class JournalArticleQueryHelper extends IndexCheckerQueryHelper {
 		Map<Long, Data> dataMap = new HashMap<Long, Data>();
 
 		Criterion criterionStatusApproved = ModelUtil.generateSQLCriterion(
-			"status=" + WorkflowConstants.STATUS_APPROVED + " or status=" +
-				WorkflowConstants.STATUS_IN_TRASH);
+			"(status=" + WorkflowConstants.STATUS_APPROVED + " or status=" +
+				WorkflowConstants.STATUS_IN_TRASH + ")");
 
 		addMissingJournalArticles(
 			model, attributesToQueryArr, criterion, criterionStatusApproved,
 			dataMap);
 
 		Criterion criterionStatusNotApproved = ModelUtil.generateSQLCriterion(
-			"status<>" + WorkflowConstants.STATUS_APPROVED + " or status<>" +
-				WorkflowConstants.STATUS_IN_TRASH);
+			"(status<>" + WorkflowConstants.STATUS_APPROVED + " or status<>" +
+				WorkflowConstants.STATUS_IN_TRASH + ")");
 
 		addMissingJournalArticles(
 			model, attributesToQueryArr, criterion, criterionStatusNotApproved,
