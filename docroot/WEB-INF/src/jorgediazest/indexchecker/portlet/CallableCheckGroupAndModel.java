1d67decb762ac41bf2b72b83b11754c43aef6159
Jorge Díaz <jorge.diaz@liferay.com>
2019-09-19 18:42:34 +0200

    Ignore nulls during comparison

--- docroot/WEB-INF/src/jorgediazest/indexchecker/portlet/CallableCheckGroupAndModel.java
+++ docroot/WEB-INF/src/jorgediazest/indexchecker/portlet/CallableCheckGroupAndModel.java
@@ -172,6 +172,8 @@ public class CallableCheckGroupAndModel implements Callable<Comparison> {
 			DataComparator exactDataComparator =
 				new DataIndexCheckerModelComparator(exactAttributesList);
 
+			exactDataComparator.setIgnoreNulls(true);
+
 			return ComparisonUtil.getComparison(
 				model, exactDataComparator, liferayData, indexData,
 				showBothExact, showBothNotExact, showOnlyLiferay,
