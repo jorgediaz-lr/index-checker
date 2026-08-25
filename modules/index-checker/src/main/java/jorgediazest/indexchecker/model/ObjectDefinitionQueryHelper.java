edc194de39e7f36cbfd99c5df7edd0e31d71a5e7
Jorge Díaz <jorge.diaz@liferay.com>
2026-08-25 14:51:05 +0200

    Filter object entries by their object definition
    All the object entries of every object definition share the ObjectEntry
    table, so the object definition has to be part of the Liferay query.
    
    Liferay names the custom object models
    com.liferay.object.model.ObjectDefinition#<shortName>, where the short
    name is a four character sequence such as T8D5, so Long.parseLong() of
    the suffix always threw NumberFormatException. That exception was
    swallowed by an empty catch block, the objectDefinitionId criterion was
    never added, and the query returned the entries of every object
    definition. They were then reported as not indexed, because the index
    side is correctly restricted to one object definition: a site scoped
    object with a single entry reported the 67 entries of the cookie consent
    system objects as missing from the index.
    
    Resolve the identifier through the className column of ObjectDefinition
    instead, which stores the whole model class name, and keep reading it
    from the suffix for the older releases that used the identifier there. No
    longer hide a failed lookup either: returning the whole table silently is
    what produced the wrong results, so let it fail and be reported as an
    error for that entity.

--- modules/index-checker/src/main/java/jorgediazest/indexchecker/model/ObjectDefinitionQueryHelper.java
+++ modules/index-checker/src/main/java/jorgediazest/indexchecker/model/ObjectDefinitionQueryHelper.java
@@ -14,16 +14,22 @@
 
 package jorgediazest.indexchecker.model;
 
+import com.liferay.petra.string.StringPool;
+
 import com.liferay.portal.kernel.dao.orm.Criterion;
+import com.liferay.portal.kernel.util.GetterUtil;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import jorgediazest.indexchecker.util.ConfigurationUtil;
 
 import jorgediazest.util.data.Data;
 import jorgediazest.util.model.Model;
+import jorgediazest.util.model.ModelFactory;
 import jorgediazest.util.model.ModelUtil;
 import jorgediazest.util.query.Query;
 
@@ -40,19 +46,15 @@ public class ObjectDefinitionQueryHelper extends IndexCheckerQueryHelper {
 
 		String className = model.getClassName();
 
-		String[] classNameArr = className.split("#");
+		if (className.indexOf(StringPool.POUND) != -1) {
+			long objectDefinitionId = _getObjectDefinitionId(className, model);
 
-		if (classNameArr.length == 2) {
-			try {
-				long objectDefinitionId = Long.parseLong(classNameArr[1]);
+			Criterion objectDefinitionIdCriterion =
+				model.getAttributeCriterion(
+					"objectDefinitionId", objectDefinitionId);
 
-				criterion = ModelUtil.generateConjunctionCriterion(
-					criterion,
-					model.getAttributeCriterion(
-						"objectDefinitionId", objectDefinitionId));
-			}
-			catch (NumberFormatException numberFormatException) {
-			}
+			criterion = ModelUtil.generateConjunctionCriterion(
+				criterion, objectDefinitionIdCriterion);
 		}
 
 		Collection<String> attributesToQuery =
@@ -62,6 +64,73 @@ public class ObjectDefinitionQueryHelper extends IndexCheckerQueryHelper {
 			new String[0]);
 
 		return Query.getData(model, attributesToQueryArr, criterion);
+	}
+
+	/**
+	 * Returns the object definition identifier of a custom object model.
+	 *
+	 * <p>
+	 * All the object entries of every object definition share the same table,
+	 * so the object definition has to be part of the query. Otherwise the
+	 * entries of all the object definitions are returned and reported as not
+	 * indexed.
+	 * </p>
+	 *
+	 * <p>
+	 * Liferay names these models
+	 * <code>com.liferay.object.model.ObjectDefinition#&lt;shortName&gt;</code>,
+	 * where the short name is a four character sequence such as
+	 * <code>T8D5</code>, so the suffix cannot be parsed as an identifier. Older
+	 * releases used the identifier itself, which is still honored.
+	 * </p>
+	 */
+	private long _getObjectDefinitionId(String className, Model model)
+		throws Exception {
+
+		String[] classNameArr = className.split(StringPool.POUND);
+
+		if (classNameArr.length != 2) {
+			throw new IllegalArgumentException(
+				"Unable to obtain the object definition of " + className);
+		}
+
+		long objectDefinitionId = GetterUtil.getLong(classNameArr[1]);
+
+		if (objectDefinitionId > 0) {
+			return objectDefinitionId;
+		}
+
+		ModelFactory modelFactory = model.getModelFactory();
+
+		Model objectDefinitionModel = modelFactory.getModelObject(
+			classNameArr[0]);
+
+		if (objectDefinitionModel == null) {
+			throw new IllegalStateException(
+				"Unable to obtain the model of " + classNameArr[0]);
+		}
+
+		Criterion criterion = objectDefinitionModel.getAttributeCriterion(
+			"className", className);
+
+		String primaryKeyAttribute =
+			objectDefinitionModel.getPrimaryKeyAttribute();
+
+		Map<Long, Data> dataMap = Query.getData(
+			objectDefinitionModel, new String[] {primaryKeyAttribute},
+			criterion);
+
+		Set<Long> objectDefinitionIds = dataMap.keySet();
+
+		if (objectDefinitionIds.size() != 1) {
+			throw new IllegalStateException(
+				"Found " + objectDefinitionIds.size() +
+					" object definitions of " + className);
+		}
+
+		Iterator<Long> iterator = objectDefinitionIds.iterator();
+
+		return iterator.next();
 	}
 
 	protected boolean indexAllVersions;
